96f92c655ab8d1b5dab467cdfccf92ac735865f9
GiovanniOuvenis <s_second@hotmail.com>
2022-04-03 15:42:53 +0200

    clearingrows

--- src/Layout/Table.js
+++ src/Layout/Table.js
@@ -2,22 +2,64 @@ import React, { useState,  useContext, useLayoutEffect} from 'react';
 import { Square } from "../Pieces/Square";
 import { TetrisContext} from "../TetrisContext";
 
-function Table() {      
-   const tableContext = useContext(TetrisContext);   
-   const arrToRender = tableContext.tab;
-   
-    
-    
-   
+
+
+ function Table() {   
+   const tableContext = useContext(TetrisContext);  
+   const borderAndBackgroundColors = tableContext.currCol;
+   const squaresToPaint = tableContext.initialPosition;
+   const tableOccupied = tableContext.occ;    
+   const [contents, setContents] = useState([]);
+
+  useLayoutEffect(()=>{
+    setContents((contents)=> {
+      contents.length = 0;
+      const {borCol, backCol} = borderAndBackgroundColors;     
+      let objToPush = {};
+  for (let s=0; s <= 219; s++) {
+   if (squaresToPaint.includes(s)) {
+      objToPush = {
+        num: s,
+        brdr: borCol,
+        bck: backCol
+      }
+    } else {
+        objToPush = {
+          num: s,
+          brdr: "red",
+          bck: "black"
+        } 
+      }
+      contents.push(objToPush);
+    } 
+    for (let o=0; o < tableOccupied.length; o++) {
+      let ind = tableOccupied[o].num;
+      contents[ind] = tableOccupied[o];
+    }
+    return contents;
+  })
+  },[squaresToPaint])
+
   
+  
+
+
   return (
     <div className='table-container'>      
       <ul className='table'>
-      {arrToRender.map((item,index) => {
-        return <Square back={item.bck} brd={item.brdr} key={index} ></Square>
+      {contents.map((item,index) => {
+        return <Square back={item.bck} brd={item.brdr} key={index} numberId={item.num}></Square>
       })}
       </ul>
       </div>
   )
 }
+
 export { Table };
+
+    
+  
+    
+         
+     
+   
--- src/TetrisContext.js
+++ src/TetrisContext.js
@@ -1,201 +1,401 @@
-
 import React, {useState, useEffect, useRef} from 'react';
-
 
 
 const TetrisContext = React.createContext();
 
-function TetrisProvider({children}) {
-      
-    const [currentColor, setCurrentColor] = useState({});    
-    const [table, setTable] = useState([]); 
-    const [pickedPiece, setPickedPiece] = useState([]);       
-    const [trigger, setTrigger ] = useState(0);
-    const [occupied, setOccupied] = useState([]);
-    const indexOfDisplayed = useRef([0]);
-       
-    
-    
-function pickRandomColor() {
-    const choices =  ["blue","blueviolet","orangered","olivedrab","fuchsia","forestgreen","firebrick","red","orange","orchid","greenyellow","green","goldenrod","wheat","white","brown"]
-    const integer = choices.length;
-    const colorNum = Math.floor(Math.random() * integer);
-    return choices[colorNum];
-    };   
-        
-function settingColors(stateArg) {
-    stateArg.borCol = pickRandomColor();
-    stateArg.backCol = pickRandomColor();
-    return stateArg;
-    };
-    
+function TetrisProvider({children}) {      
+    const [currentColor, setCurrentColor] = useState({});
+    const [positionOfCurrentPiece, setPositionOfCurrentPiece] = useState([]);
+    const [occupied, setOccupied] = useState([]);   
+    const [seconds, setSeconds] = useState(1500); 
+    const [trigger, setTrigger] = useState(false); 
+    const [score, setScore] = useState("0000005");    
+    const contextRef = useRef([0]);
+  
+
+    function pickRandomColor() {
+        const choices =  ["blue","blueviolet","orangered","olivedrab","fuchsia","forestgreen","firebrick","red","orange","orchid","greenyellow","green","goldenrod","wheat","white","brown"]
+        const integer = choices.length;
+        const colorNum = Math.floor(Math.random() * integer);
+        return choices[colorNum];
+     };   
+     function pickRandomPositionsOfPiece() {
+        const positions = [[25,15,35,5],[15,4,5,16],[14,4,5,13],[15,4,5,25],[15,5,16,25],[15,5,6,25],[14,4,15,5]]
+        const int = positions.length;
+        const positionsInt = Math.floor(Math.random() * int);
+        let picked = positions[positionsInt]
+        return picked;
+      };
+      function settingColors(stateArg) {
+        stateArg.borCol = pickRandomColor();
+        stateArg.backCol = pickRandomColor();
+        return stateArg;
+      };
+      function recursiveCount(numArg, arrArg) {
+         if (!arrArg.includes(numArg)) {
+             return false;
+         } else if ((numArg + 1) % 10 === 0) {
+             return Math.floor(numArg / 10) * 10;
+         } else {
+             return recursiveCount(numArg+1,arrArg)
+         }
+      }
+     
 
 useEffect(()=> {    
-    setCurrentColor((currentColor)=> {
-    return settingColors(currentColor)
-    })
-    }, [])    
-    
-  
-useEffect(()=>{
-    setTable((table)=>{
-       for (let t=0; t<= 219; t++) {
-        table.push({
-            brdr: "red",
-            bck: "black"
-            })
-        }
-        return table;
-    })
-    },[])          
-    
+     setCurrentColor((currentColor)=> {
+     return settingColors(currentColor)
+     })
+    }, [occupied])    
 
-useEffect(()=>{
-   setPickedPiece((pickedPiece)=>{
-     const pieces = [
-                 
-         [
-             [4,5,14,13],
-             [15,25,14,4],
-             [24,23,14,15],
-             [13,3,14,24]
-         ], // s
-         [
-             [4,5,15,16],
-             [6,16,15,25],
-             [26,25,15,14],
-             [24,14,15,5] 
-         ], // z
-         [ 
-             [5,15,25,26],
-             [16,15,14,24],
-             [25,15,5,4],
-             [14,15,16,6]
-         ], // L
-         [
-           [5,15,25,24],
-           [16,15,14,4],
-           [25,15,5,6],
-           [14,15,16,26]  
-         ], // Inverted L
-         [
-           [5,14,15,16],
-           [16,5,15,25],
-           [25,16,15,14],
-           [14,25,15,5]// .:.  
-         ]
 
-     ] 
-    
-     let num = Math.floor(Math.random() * 5);
-     return pieces[num];
-
-   })
-},[indexOfDisplayed.current])
-
-useEffect(()=>{
-    
-setTable((table)=>{
-      //   console.log(pickedPiece[indexOfDisplayed.current])
-    if (!pickedPiece[indexOfDisplayed.current[0]]) {
-        return table;
-     } 
-    for (let p=0; p <= pickedPiece[indexOfDisplayed.current[0]].length - 1; p++) {
-           let ind = pickedPiece[indexOfDisplayed.current[0]][p]; 
-           table[ind].brdr = currentColor.borCol;
-           table[ind].bck = currentColor.backCol;   
-            }              
-         return table;         
-    }
-      )     
-},[pickedPiece, indexOfDisplayed.current]) 
-            
-
-useEffect(()=>{
-    const increaseRepeatedly = setInterval(()=>{
-      setPickedPiece((pickedPiece)=>{
-        let plusTen = pickedPiece.map((pp)=>{
-            return pp.map((pt)=>{
-                return pt + 10;
-            })
-        })        
-        return plusTen;
-      })     
-           
-           
-    }, 1500)
-    return ()=> clearInterval(increaseRepeatedly);
-}, [])
-
-/*useEffect(()=>{
-  setTrigger((trigger)=>{
-     
+useEffect(()=> {  
+  setPositionOfCurrentPiece((positionOfCurrentPiece) => {
+    let result = pickRandomPositionsOfPiece();
+    return result;  
   })
-},[numberOfReference])*/
+}, [occupied])
+
+
+
+  
+       
+     
+
+
+ useEffect(() => {
+    const rightLeftDownKey = (e) => {
+       
+        setPositionOfCurrentPiece((positionOfCurrentPiece)=> {
+            
+            if (e.keyCode === 39) {
+                
+                let onePieceRight = positionOfCurrentPiece.some((pos) => {
+                  if ((pos + 1) % 10 === 0) {
+                    return true;
+                } 
+                if (contextRef.current.includes(pos + 1)) {
+                    return true;
+                }
+                
+                })
+                
+            let right = positionOfCurrentPiece.map((re)=>{
+                return re + 1;
+            })  
+            
+            if (onePieceRight) {
+                return positionOfCurrentPiece;                
+            } 
+            else {
+                return right;
+            }
+            } else if (e.keyCode === 37) {
+    
+            e.preventDefault();
+            let conditionToMove = positionOfCurrentPiece.some((piec) => {
+             if (piec % 10 === 0) {
+                 return true;
+             } 
+             if (contextRef.current.includes(piec -1)) {
+                return true;
+            }
+
+            })
+            let onePieceLeft = positionOfCurrentPiece.map(((lp) => {
+                return  lp - 1;
+            }))
+         
+        if (conditionToMove) {
+             return positionOfCurrentPiece;
+        } else {
+             return onePieceLeft;
+        }
+    } 
+    else if (e.keyCode === 40) {       
+        e.preventDefault();
+        // 
+        if (trigger) {
+            return positionOfCurrentPiece;
+        }
+        let onePieceDown = positionOfCurrentPiece.map((arg) => {
+          return  arg + 10;       
+        })       
+        let collision = positionOfCurrentPiece.some((col) =>{
+            if (col >= 210) {
+              return true;
+            } 
+            if (contextRef.current.includes(col + 10)) {
+              return true;
+            }
+        })
+        
+     if (collision) {       
+
+       return  positionOfCurrentPiece.concat(positionOfCurrentPiece[0]);
+     } else {
+        return onePieceDown;
+     }
+        
+    } else if (e.keyCode === 38) {
+        e.preventDefault();
+        let newArray = [];
+        for (let r=0; r < positionOfCurrentPiece.length; r++){
+            newArray[0] = positionOfCurrentPiece[0];     
+
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] - 10) {
+                newArray[r] = positionOfCurrentPiece[0]  +1;
+            }
+
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] - 10 + 1) {
+                newArray[r] = positionOfCurrentPiece[0] + 10 + 1;
+            }
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] + 1) {
+                newArray[r] = positionOfCurrentPiece[0] + 10;
+            }
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] + 10 + 1) {
+                newArray[r] = positionOfCurrentPiece[0] + 10 -1;
+            }
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] + 10) {
+                newArray[r] = positionOfCurrentPiece[0] - 1;
+            }
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] + 10 - 1) {
+                newArray[r] = positionOfCurrentPiece[0] - 10 - 1;
+            }
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] - 1) {
+                newArray[r] = positionOfCurrentPiece[0] - 10;
+            }
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] - 10 - 1) {
+                newArray[r] = positionOfCurrentPiece[0] - 10 + 1;
+            }  
+
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] - 20) {
+                newArray[r] = positionOfCurrentPiece[0] + 2;
+            }
+
+            if (positionOfCurrentPiece[r] === positionOfCurrentPiece[0] + 2) {
+                newArray[r] = positionOfCurrentPiece[0] - 20;
+            }
+                               
+            
+        }
+       
+        let checkCondition = newArray.some((item)=>{
+           if (item >=210) {
+               return true;
+           }
+           if (contextRef.current.includes(item + 10)) {
+               return true;
+           }
+        })
+        
+         let superImpose = newArray.some((rot)=>{
+             if (contextRef.current.includes(rot)) {
+                 return true;
+             } else {
+                 return false;
+             }
+         })
+         
+          let isBarOnRightMargin = positionOfCurrentPiece.every((ir) => {
+              return (ir + 1) % 10 === 0;
+          })
+          if (isBarOnRightMargin) {
+            let minusTwo = newArray.map((mt)=> {
+                return mt - 2;
+            })
+            return minusTwo;
+        }         
+       if (superImpose) {
+        return positionOfCurrentPiece;
+       }        
+        if (positionOfCurrentPiece[0] - positionOfCurrentPiece[1] === 10 && positionOfCurrentPiece[2] - positionOfCurrentPiece[3] === 10) {
+            return positionOfCurrentPiece;
+        }       
+       if (positionOfCurrentPiece[0] >= 210 || contextRef.current.includes(positionOfCurrentPiece[0] + 10)) {
+            let minusTen = newArray.map((mt) => {
+                return mt - 10;
+            })
+            return minusTen.concat(positionOfCurrentPiece[0],positionOfCurrentPiece[0]);
+        }        
+       if (positionOfCurrentPiece[0] % 10 === 0 || contextRef.current.includes(positionOfCurrentPiece[0] -1)) {
+            let plusOne = newArray.map((po)=>{
+                return po + 1;
+            }) 
+            return plusOne;
+       }  
+       if ((positionOfCurrentPiece[0] + 1) % 10 === 0) {
+           let minusOne = newArray.map((mo) => {
+               return mo - 1;
+           })
+           return minusOne;
+       }   
+       if (checkCondition) {
+           return newArray.concat(positionOfCurrentPiece[0],positionOfCurrentPiece[0]);
+       } else {
+           return newArray;
+       }
+    }            
+})
+}
+window.addEventListener("keyup", rightLeftDownKey);
+
+}, [])
+        
+useEffect(()=> {    
+       let getIndexes = occupied.map((ci)=>{
+           return ci.num;
+       });           
+       contextRef.current = getIndexes     
+       return contextRef;
+   
+   },[occupied]);            
+      
+ 
+
+ useEffect(()=> {
+    setOccupied((occupied) => {           
+       
+    if  (positionOfCurrentPiece.length >= 6) {
+        let objectsToConcat = positionOfCurrentPiece.map((newOccupiedCell)=> {
+        return { num: newOccupiedCell, brdr: currentColor.borCol, bck: currentColor.backCol}
+        })
+        let newOccupied = occupied.concat(objectsToConcat);
+        let indexes = newOccupied.map((indx)=>{
+            return indx.num;
+        })
+        let minVal = Math.min.apply(null,indexes);
+        let rows = [];
+        for (let r=210; r >=minVal; r-=10) {
+            let row = recursiveCount(r,indexes);
+            if (row) {
+                rows.push(row);
+            }
+        }
+        contextRef.current[0] = rows.length * 1000;
+        
+        let newFiltered = newOccupied.filter((itf)=>{
+            let floored = Math.floor((itf.num) / 10) * 10;
+            let times = 0;
+            for (let z=0; z< rows.length; z++) {
+                if (itf.num < rows[z]) {
+                    times += 10;
+                }
+            }
+            itf.num += times;
+            return !rows.includes(floored);
+        })
+        
+          return newFiltered;
+        } else {
+          return occupied;
+        }            
+       })
+
+ },[positionOfCurrentPiece])     
+
+
+
 
 
 useEffect(()=>{
-    const handleArrowButtons = (e) => {
-setPickedPiece((pickedPiece)=>{
-    if (e.keyCode === 37) {   
-             
-        let minusOne = pickedPiece[indexOfDisplayed.current[0]].map((mo)=>{return mo -1})
-        pickedPiece[indexOfDisplayed.current[0]].length = 0;
-        pickedPiece[indexOfDisplayed.current[0]] = [...minusOne];
-        return pickedPiece;
-    } 
-    if(e.keyCode === 38) {          
-        if (indexOfDisplayed.current[0] === 3) {
-            indexOfDisplayed.current[0] = 0;
-            return indexOfDisplayed.current;
-        } else {
-            indexOfDisplayed.current[0] += 1;
-        }       
-    }
-    if (e.keyCode === 39) {      
-        let plusOne = pickedPiece[indexOfDisplayed.current[0]].map((po)=>{ return po + 1}) 
-        pickedPiece[indexOfDisplayed.current[0]].length = 0;
-        pickedPiece[indexOfDisplayed.current[0]] = [...plusOne];     
-        return pickedPiece;
-    }
-    if (e.keyCode === 40) {
-        let plusten = pickedPiece[indexOfDisplayed.current[0]].map((pten)=>{ return pten + 10})
-        pickedPiece[indexOfDisplayed.current[0]].length = 0;
-        pickedPiece[indexOfDisplayed.current[0]] = [...plusten];
-        return pickedPiece;
-    }
+    setTrigger((trigger)=> {
+     let condition = positionOfCurrentPiece.some((pocp)=>{
+         return contextRef.current.includes(pocp)})
+    return condition ? true : false;
+    })
+  },[positionOfCurrentPiece])
 
-
-})
-       
-       
-       
-    }
-        window.addEventListener("keyup", handleArrowButtons);
-    },[])
+  
         
         
-    
+        
+useEffect(()=> {
+       const belated = setInterval(()=> {
+          setPositionOfCurrentPiece((positionOfCurrentPiece)=>{   
+                   
+          let check =  positionOfCurrentPiece.some((elem) => {
+                if (contextRef.current.includes(elem + 10)) {
+                    return true;
+                } 
+                if (elem < 210) {
+                    return false;
+                } else {
+                    return true;
+                }
+            })
+          let plusTen = positionOfCurrentPiece.map((pop) => {
+              return pop + 10;
+          })
+          if (trigger) {
+              return positionOfCurrentPiece;
+          }
+          if (check) {          
+              let itemToConcat = positionOfCurrentPiece[0];    
+              let newPositions = positionOfCurrentPiece.concat(itemToConcat);
+            return newPositions;
+          } else {
+              return plusTen;
+          }
+            
+          }) 
+        },seconds);
+        return () => clearInterval(belated);
+        }, [seconds,trigger]
+        ) 
+
+  useEffect(()=> {
+            const changeSpeed = setInterval(()=> {
+               setSeconds((seconds)=>{   
+                return seconds <= 500 ? seconds : seconds - 200;                               
+               }) 
+             },180000);
+             return () => clearInterval(changeSpeed);
+             }, []
+             ) 
+
+
+
+useEffect(()=>{
+  setScore((score)=>{
+      let parsed = parseInt(score);               
+      console.log(contextRef.current[0]) 
+      let newScore = parsed + 5 + contextRef.current[0];
+     /* if (typeof contextRef.current[0] === "number") {
+          newScore += contextRef.current[0]
+      }*/
+      let stringedLength = newScore.toString().length; 
+      let digits = 7 - stringedLength; 
+      let arrToJoin = [];
+      arrToJoin.length = 7;
+      arrToJoin.fill(0,0,digits);
+      arrToJoin.push(newScore); 
+         if (trigger) {
+             return score;
+         } else {
+      return arrToJoin.join("");
+         }
+  })
+},[occupied])          
+
+         
+return (
+    <TetrisContext.Provider ref={contextRef} value={{
+    refer : contextRef,
+    initialPosition: positionOfCurrentPiece, 
+    currCol : currentColor,        
+    occ: occupied,                           
+    trigg: trigger,
+    points : score
+}}>
+    {children}
+</TetrisContext.Provider>
+    )
+}
+const TetrisConsumer =  TetrisContext.Consumer;   
+
+export { TetrisProvider, TetrisContext, TetrisConsumer}    
            
-   
- 
-      
-        
-      
-       return (
-           <TetrisContext.Provider  ref = {indexOfDisplayed} value={{
-               refer: indexOfDisplayed,                         
-               tab : table,             
-               currCol : currentColor,               
-               pp: pickedPiece                     
-           }}>
-               {children}
-           </TetrisContext.Provider>
-               )
-           }
-           const TetrisConsumer =  TetrisContext.Consumer;   
-       
-           export { TetrisProvider, TetrisContext, TetrisConsumer}    
 
 
     
